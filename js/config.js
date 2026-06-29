596c3efe222b9d662027e2cc1e9782a969bfb54e
LucasFerreiraCSilva <126568519+LucasFerreiraCSilva@users.noreply.github.com>
2026-06-29 10:19:08 -0300

    Update Supabase URL to use HTTPS

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
    ============================================ */
 
 const CONFIG = {
-  SUPABASE_URL:     'olonfkxativeuxqemdzv.supabase.com',   
+  SUPABASE_URL:     'https://olonfkxativeuxqemdzv.supabase.co',   
   SUPABASE_KEY:     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sb25ma3hhdGl2ZXV4cWVtZHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MjkwODgsImV4cCI6MjA5ODEwNTA4OH0.DQ9HU-xAopsyR0CP5SRZD0_R-e0QbI6-c8PH5ueh2nU',       
 
   MSG_TIMEOUT: 5000
